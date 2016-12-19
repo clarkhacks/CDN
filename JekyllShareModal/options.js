f2c887cd8feb0ece96c7d75817bf685b4d552d6e
Clark Weckmann <clarkhacks@users.noreply.github.com>
2016-12-19 13:07:50 -0600

    Create options.js

---
+++ JekyllShareModal/options.js
@@ -0,0 +1,12 @@
+var options = {
+    "sTitle": "1",
+    "customTitle": "Share",
+    "fb": "1",
+    "tumblr": "1",
+    "gplus": "1",
+    "email": "1",
+    "twitter": "1",
+    "Embed": "A",
+    "cEmbed": "",
+    "warningLabel": "The embed code reqiures that you have Bootstrap on your site."
+};
