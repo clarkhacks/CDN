e59e9353d292906623c8d47b668c69a241bfdea7
Clark Weckmann <clarkhacks@users.noreply.github.com>
2017-01-26 07:54:59 -0600

    Update index.js

--- JekyllShareModal/index.js
+++ JekyllShareModal/index.js
@@ -1,18 +1,29 @@
-var shrModal = "<div id=\"shareModal\" class=\"modal fade\" role=\"dialog\"> <div class=\"modal-dialog\"> <div class=\"modal-content\"> <div class=\"modal-header modal-header-share\"> <button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;<\/button> <span class=\"modal-title\" id=\"share-modal-title\"><\/span> <\/div><div class=\"post-share-modal modal-body\"> <div id=\"shr-btn-wrap\"> <\/div><hr> <div> <h3>Post Address<\/h3> <p> A direct link to this post! <\/p><input type=\"text\" id=\"windowurl\"\/> <a href=\"#\" data-copytarget=\"#windowurl\" class=\"btn btn-primary\">Copy<\/a> <\/div><hr><p class=\"embed-post\" id=\"embed-post-block\"><\/p><\/div><\/div><\/div><\/div>";
-document.write(shrModal);
 var windowLoc = window.location.href;
+var qrBTN = '<button type=\"button\" class=\"btn btn-primary\" style=\"display: block; width: 100%\" data-toggle=\"modal\" data-target=\"#qrModal\">Generate QR<\/button>';
+
 document.querySelector("input").setAttribute("value", windowLoc);
+
 function extract(variable) {
     for (var key in variable) {
         window[key] = variable[key];
     }
 }
-extract(options);
 
+extract(jkShrOptions);
+
+// title set
 if (sTitle == 1) {
     document.getElementById("share-modal-title").innerHTML = ("<h2>" + customTitle + "<\/h2>");
 } else {
     console.log("no title");
+}
+
+// Buttons Set
+
+if (QR == 1) {
+    document.getElementById("qr-btn").innerHTML = (qrBTN);
+} else {
+    console.log("qr = 0");
 }
 if (fb == 1) {
     document.getElementById('shr-btn-wrap').insertAdjacentHTML('beforeend', '<a  href=\"http:\/\/www.facebook.com\/sharer\/sharer.php?u={{ site.url }}{{ page.url }}&title={{ page.title }}\" class=\"btn btn-social btn-facebook\"><span class=\"fa fa-facebook\"><\/span>Facebook<\/a>');
@@ -40,9 +51,13 @@ if (twitter == 1) {
 } else {
     console.log("twitter = 0");
 }
+if (instapaper == 1) {
+  document.getElementById("shr-btn-wrap").insertAdjacentHTML('beforeend', '');
+}
 if (Embed == "A") {
-    document.getElementById("embed-post-block").innerHTML = ("<h3>Embed Post<\/h3><div id=\"warning-embed\"><\/div><div class=\"embed-post-text\"><textarea spellcheck=\"false\" name=\"embed\" rows=\"4\" id=\"embedtext\">&lt;div class=\"well post-card\"&gt; &lt;img style=\"width: 100%; border-radius: 4px; margin-bottom: 10px;\" src=\"{{ site.url }}\/a\/img\/post\/covers\/bootstrapcover.png\" alt=\"{{site.title}}\"\/&gt; &lt;h4 style=\"text-align: center\"&gt;{{ post.title }}&lt;\/h4&gt; &lt;p&gt;{{ page.desc }}&lt;\/p&gt;&lt;a href=\"{{ site.baseurl }}{{ page.url }}\" style=\"position: relative; display: block; width: 80%; margin-left: 10%; margin-right: auto\" class=\"btn btn-danger\"&gt;Read&lt;\/a&gt;&lt;\/div&gt;<\/textarea><\/div><a href=\"#\" data-copytarget=\"#embedtext\" class=\"btn btn-primary\">Copy<\/a>");
-} else if (Embed === 0) {
+  document.getElementById("embed-post-block").innerHTML = ("<h3>Embed Post<\/h3><div id=\"warning-embed\"><\/div><div class=\"embed-post-text\"><textarea spellcheck=\"false\" name=\"embed\" rows=\"4\" id=\"embedtext\">&lt;div class=\"well post-card\"&gt; &lt;img style=\"width: 100%; border-radius: 4px; margin-bottom: 10px;\" src=\"{{ site.url }}\/a\/img\/post\/covers\/bootstrapcover.png\" alt=\"{{site.title}}\"\/&gt; &lt;h4 style=\"text-align: center\"&gt;{{ post.title }}&lt;\/h4&gt; &lt;p&gt;{{ page.desc }}&lt;\/p&gt;&lt;a href=\"{{ site.baseurl }}{{ page.url }}\" style=\"position: relative; display: block; width: 80%; margin-left: 10%; margin-right: auto\" class=\"btn btn-danger\"&gt;Read&lt;\/a&gt;&lt;\/div&gt;<\/textarea><\/div><a href=\"#\" data-copytarget=\"#embedtext\" class=\"btn btn-primary\">Copy<\/a>");
+}
+else if (Embed === 0) {
     console.log("Embed Code Disabled");
 } else {
     document.getElementById("embedtext").innerHTML = ("<h3>Embed Post<\/h3><div id=\"warning-embed\"><\/div><div class=\"embed-post-text\"><textarea name=\"embed\" rows=\"4\" id=\"embedtext\">" + cEmbed + "<\/textarea><\/div><a href=\"#\" data-copytarget=\"#embedtext\" class=\"btn btn-primary\">Copy<\/a>");
@@ -52,21 +67,38 @@ if (warningLabel === 0) {
 } else {
     document.getElementById("warning-embed").innerHTML = ("<p>" + warningLabel + "<\/p>");
 }
+
+/*
+	Copy text from any appropriate field to the clipboard
+*/
 (function() {
 
     'use strict';
+
+    // click events
     document.body.addEventListener('click', copy, true);
+
+    // event handler
     function copy(e) {
+
+        // find target element
         var
-        t = e.target,
+            t = e.target,
             c = t.dataset.copytarget,
             inp = (c ? document.querySelector(c) : null);
+
+        // is element selectable?
         if (inp && inp.select) {
+
+            // select text
             inp.select();
 
             try {
+                // copy text
                 document.execCommand('copy');
                 inp.blur();
+
+                // copied animation
                 t.classList.add('copied');
                 setTimeout(function() {
                     t.classList.remove('copied');
@@ -80,5 +112,3 @@ if (warningLabel === 0) {
     }
 
 })();
-
-
