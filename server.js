3b8576b4916b0b431354be51adc556315ef3fc70
Clark Weckmann <cweckmaniii@gmail.com>
2017-10-28 13:38:08 -0500

    + bg.jpg

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
  var express = require('express');
 var path = require('path');
-var app = express();
-// Define the port to run on
-app.set('port', 8081);
-app.use(express.static(path.join(__dirname, '/')));
+var appAPPS = express();
+var appCDN = express();
+var appRECYCLE = express();
+// CDN Server
+appCDN.set('port', 8081);
+appCDN.use(express.static(path.join(__dirname, '/CDN')));
+var server = appCDN.listen(appCDN.get('port'), function() {
+  var port = server.address().port;
+  console.log('CDN Serving Locally on ' + port);
+});
+
+// APPS Server
+appAPPS.set('port', 8082);
+appAPPS.use(express.static(path.join(__dirname, '/APPS')));
+var server = appAPPS.listen(appAPPS.get('port'), function() {
+  var port = server.address().port;
+  console.log('CDN Serving Locally on ' + port);
+});
+
+// Recycle Server
+appRECYCLE.set('port', 8083);
+appRECYCLE.use(express.static(path.join(__dirname, '/Recycle')));
 
 // Listen for requests
-var server = app.listen(app.get('port'), function() {
+var server = appRECYCLE.listen(appRECYCLE.get('port'), function() {
   var port = server.address().port;
-  console.log('Magic happens on port ' + port);
+  console.log('CDN Serving Locally on ' + port);
 });
