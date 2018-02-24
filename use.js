a526f1d4d4fc79bdbe816c5981305fd3a143c4ea
Clark Weckmann <clarkhacks@users.noreply.github.com>
2018-02-24 09:19:33 -0600

    Create use.js

---
+++ use.js
@@ -0,0 +1,10 @@
+function useJs(url, callback)
+{
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.onreadystatechange = callback;
+    script.onload = callback;
+    head.appendChild(script);
+}
